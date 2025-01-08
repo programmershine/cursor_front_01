fdeed11ed8210ff18d6446622a2084a3482e6d83
programmershine <dsrshinekim@gmail.com>
2025-01-08 15:11:30 +0900

    yes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/Home';
-import PrivateRoute from './components/PrivateRoute';
-import BoardList from './components/Board/BoardList';
-import BoardDetail from './components/Board/BoardDetail';
-import BoardWrite from './components/Board/BoardWrite';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/board" element={<BoardList />} />
-          <Route path="/board/:id" element={<BoardDetail />} />
-          <Route path="/board/write" element={<BoardWrite />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
